895fc65507a8b6c80372e3dfea8a7624d400b59b
Augusto Araújo <augustoaraujo4520@gmail.com>
2021-01-25 14:26:14 -0300

    Update BancoDeDados.java

--- BancoDeDados.java
+++ BancoDeDados.java
@@ -5,8 +5,7 @@ public class BancoDeDados {
     public static void main(String[] args) {
        
         /*
-        ESTANCIANDO O MEU BANCO
-        
+        ESTANCIANDO o BANCO
         */
         
          Conexao c = new Conexao();
